543099e9e7665769c4d4b2512d5da2b7b2be916a
Andres Foronda <andresforonda.af@gmail.com>
2021-02-15 22:25:39 -0300

    Change the camera position property to show the cube

--- 03-basic-scene/exercise/script.js
+++ 03-basic-scene/exercise/script.js
@@ -80,7 +80,9 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); // h
  * - Scale
  */
 
-camera.position.z = 2;
+camera.position.z = 3;
+// camera.position.x = 2;
+// camera.position.y = 1;
 scene.add(camera);
 
 /**
