a71a6037dcad41c4c1788dddd99cc7411a2df1ba
Andres Foronda <andresforonda.af@gmail.com>
2021-02-21 15:23:20 -0300

    Add using scale example to mesh

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -5,13 +5,14 @@ import { Scene } from 'three'
 /**
  * Classes that are based in the Object3D object, inherit the properties:
  *
- * - Position (move objects): position is not an object, it is a Vector3 that allows to
- *   position things in the space, it has useful methods, like length, add, ceil and more
- *   (https://threejs.org/docs/#api/en/math/Vector3)
- *
+ * - Position (move objects)
  * - Scale
  * - Rotation
  * - Quaternion (kind of rotation)
+ *
+ * Position and scale are not objects, but Vector3, which allows to
+ * position things in the space, it has useful methods, like length, add, ceil and more
+ * (https://threejs.org/docs/#api/en/math/Vector3)
  */
 
 // Canvas
@@ -26,15 +27,23 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-// Using position with mesh
+scene.add(mesh)
+
+// console.log(mesh.position.length()) // Length of the vector, distance of the center of the scene and the object's position
+
+// Using Position with mesh
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
 // Same as above (x, y, z)
 mesh.position.set(0.7, -0.6, 1)
-scene.add(mesh)
 
-console.log(mesh.position.length()) // Length of the vector, distance of the center of the scene and the object's position
+// Using Scale with mesh
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// Same as above (x, y, z)
+mesh.scale.set(2, 0.5, 0.5)
 
 /**
  * Sizes
@@ -49,8 +58,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-console.log(mesh.position.distanceTo(camera.position)) // Vector3 distanceTo (mesh to the camera)
 scene.add(camera)
+// console.log(mesh.position.distanceTo(camera.position)) // Vector3 distanceTo (mesh to the camera)
 
 // mesh.position.normalize() // Takes the vector length and reduce it to 1
 // console.log(mesh.position.length()) // Length of the vector, distance of the center of the scene and the object's position
